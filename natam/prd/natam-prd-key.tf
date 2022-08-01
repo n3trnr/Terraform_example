bacbdb8195b41e9655239a10c9f0e5f3a6383049
Ju Min Soo <71677383+n3trnr@users.noreply.github.com>
2022-08-01 14:37:55 +0900

    Update natam-prd-key.tf

--- natam/prd/natam-prd-key.tf
+++ natam/prd/natam-prd-key.tf
@@ -1,7 +1,7 @@
 
 resource "aws_key_pair" "natam-prd-key" {
   key_name   = "natam-prd-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC4FdGTEbngFIh9Kvl+QaXrTvOcNrplB93UgEyd04gUKJxfYeOc12cd8X+4kZqhFXnIS0KdF23c3caA1A8NS1EiYg/ky7XjdetYcMn0CKYD/QC82dGKv9vgn/52l3pu6xZ0Ze9Z6ooSwhQAojIUJ0Qta4x4UIoMM03Q41MJLQbiBvVS2daeellw/XbfozV+VYwwdRVhSIfCnQ12rWM9Z4uhCbazBQT8yRmK9ALLal6LeFEYI0Q7oM7Llah1zcUkOz8yH5XkSYGDvOEEXzB0IBOvzEhvB0vFTjDHcIMkLR3LMQMI2TlMD2OBIEfzJRuModjNaF3ZqWbpJN7FAHaQrUK3 refine@terraform"
+  public_key = "PUBLIC_KEY"
 	
 	tags = {
 			Name = "natam-prd-key"
