86ea4931593d55737282f175e1a9c4d601e47ceb
Ju Min Soo <71677383+n3trnr@users.noreply.github.com>
2022-08-01 14:37:08 +0900

    Update natam-dev-key.tf

--- natam/dev/natam-dev-key.tf
+++ natam/dev/natam-dev-key.tf
@@ -1,7 +1,7 @@
 
 resource "aws_key_pair" "natam-dev-key" {
   key_name   = "natam-dev-key"
-  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQDogbNC76N25iOmfuwCMcwUycGjG4uKQxfD92iWPKDiCXOHcw7Rqyt7HtXCbfqE/+KtnV0yQWebU9fHC6BRADXC5gqJWfl27kdC7Xwt9UKG+fTMDOs8EbG+YLnoKq4wDIb16cMy2lAf7rDGG0a+cLXMxbNpnpdQ6SWszNW6Pr0Vm5GU5uNnfnvcqUp6cQXNmqnYHjZ6PGSTQ8IdjPHNHWL9TouPnxT9f4JiyIoUQVSOhBLfaaXE9WHWRDn8xF0Vk3KGt1J6hzhXTMCseGZ+neT2YQdAM4gsgsXd8tgK4jdVUmkx4Js35BnLshTbKtBqe1uVUpXUqzVMbMDFEuI0xAl/ refine@terraform"
+  public_key = "PUBLIC_KEY"
 	
 	tags = {
 			Name = "natam-dev-key"
